6ec9331260653270939c1570ae8d39da4d62ea23
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-03 15:31:04 +0900

    server setting2

--- src/server.js
+++ src/server.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express()
 
-app.listen(3000)
+app.set('view engine', "pug");
+app.set('views', __dirname + "/src/views");
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen)
