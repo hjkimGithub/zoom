5a9180f2280e6e14f0ad0068c941435ff9af2e92
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-06 21:01:48 +0900

    socket io, callback

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,19 @@
 const socket = io();
 
+const welcome  = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", {payload:input.value}, () =>{
+        console.log("Server is done");
+    });
+    input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
 // const msgList = document.querySelector("ul");
 // const nickForm  = document.querySelector("#nickname");
 // const msgForm  = document.querySelector("#message");
