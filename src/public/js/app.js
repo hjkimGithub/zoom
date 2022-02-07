ca6fe0ba2bd89772bca478cea8740e6c2c690bfc
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-07 14:31:27 +0900

    camera test

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,28 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+    } catch(e) {
+        console.log(e);
+    }
+}
 
 async function getMedia() {
     try {
@@ -11,9 +31,35 @@ async function getMedia() {
             video: true,
         });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch(e) {
         console.log(e);
     }
 }
 
 getMedia();
+
+function handleMuteClick() {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+    if(!muted){
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+
+function handleCameraClick() {
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+    if(cameraOff){
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
